dc7cd9606967c5d6fcf8ce8eff6c1e72f83e4d1e
jonathan0elenilson <jonathan.elenilson@outlokk.com>
2022-07-20 19:51:14 -0600

    pagina 1 y 2 terminadas

---
+++ js/main2.js
@@ -0,0 +1,19 @@
+const $formulario = document.querySelector("#formulario"),
+        $NOMBRE = document.querySelector("#NOMBRE"),
+        $WHATSAPP = document.querySelector("#WHATSAPP");
+
+        $formulario.onsubmit = evento =>{
+        evento.preventDefault();
+
+          const NOMBRE = $NOMBRE.value,
+          WHATSAPP = $WHATSAPP.value;
+          //validad
+         if ( NOMBRE == "" && WHATSAPP == ""){
+        alert("puto")
+        //return 
+         }else{
+        alert("p20uta")
+        //return true
+        $formulario.submit();
+    }   
+};
